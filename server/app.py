918250dddd793439ffaa27a9340543181a118c6c
palindrome6 <nitinnarayan1501@gmail.com>
2023-05-15 00:38:12 +0200

    hack to get flatten and bachnorm working

--- server/app.py
+++ server/app.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask import request;
 from flask_cors import CORS, cross_origin
-from infer import infer
-from nn_def import *
+import torch.nn as nn
+from infer import FlattenConsecutive, BatchNorm1d, infer
+# ------------------------
+import __main__
+ 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 @cross_origin() 
-def hello_name():
+def hello():
+   __main__.FlattenConsecutive = FlattenConsecutive
+   __main__.BatchNorm1d = BatchNorm1d
+   
    prompt = request.args.get('name')
    names_gen = infer(prompt)
    text = ','.join(names_gen)
@@ -16,4 +22,4 @@ def hello_name():
    return text
 
 if __name__ == '__main__':
-    app.run()
+   app.run(host="0.0.0.0")
--- server/infer.py
+++ server/infer.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn.functional as F
 import torch.nn as nn
 import torch.optim as optim
-from nn_def import FlattenConsecutive, BatchNorm1d
 import random
 
 
@@ -21,8 +20,82 @@ ch_to_i['.'] = 0
 i_to_ch = {i: s for s, i in ch_to_i.items()}
 vocab_size = len(i_to_ch)
 
+
+class FlattenConsecutive(nn.Module):
+
+  def __init__(self, n):
+    super().__init__()
+    self.n = n
+    
+  def __call__(self, x):
+    B, T, C = x.shape
+    x = x.view(B, T//self.n, C*self.n)
+    if x.shape[1] == 1:
+      x = x.squeeze(1)
+    self.out = x
+    return self.out
+  
+
+class BatchNorm1d(nn.Module):
+  
+  def __init__(self, dim, eps=1e-5, momentum=0.1):
+    super().__init__()
+    self.eps = eps
+    self.momentum = momentum
+    self.training = True
+    # parameters (trained with backprop)
+    self.gamma = torch.ones(dim)
+    self.beta = torch.zeros(dim)
+    # buffers (trained with a running 'momentum update')
+    self.running_mean = torch.zeros(dim)
+    self.running_var = torch.ones(dim)
+  
+  def __call__(self, x):
+    # calculate the forward pass
+    if self.training:
+      if x.ndim == 2:
+        dim = 0
+      elif x.ndim == 3:
+        dim = (0,1)
+      xmean = x.mean(dim, keepdim=True) # batch mean
+      xvar = x.var(dim, keepdim=True) # batch variance
+    else:
+      xmean = self.running_mean
+      xvar = self.running_var
+    xhat = (x - xmean) / torch.sqrt(xvar + self.eps) # normalize to unit variance
+    self.out = self.gamma * xhat + self.beta
+    # update the buffers
+    if self.training:
+      with torch.no_grad():
+        self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * xmean
+        self.running_var = (1 - self.momentum) * self.running_var + self.momentum * xvar
+    return self.out
+  
+  def parameters(self):
+    return [self.gamma, self.beta]
+  
+
+# -------------------------
+model = nn.Sequential(
+    nn.Embedding(vocab_size, n_embd),
+    FlattenConsecutive(2), 
+    nn.Linear(n_embd * 2, n_hidden, bias=False), 
+    BatchNorm1d(n_hidden), 
+    nn.Tanh(),
+    FlattenConsecutive(2), 
+    nn.Linear(n_hidden * 2, n_hidden, bias=False), 
+    BatchNorm1d(n_hidden), 
+    nn.Tanh(),
+    FlattenConsecutive(2), 
+    nn.Linear(n_hidden * 2, n_hidden, bias=False), 
+    BatchNorm1d(n_hidden), 
+    nn.Tanh(),
+    nn.Linear(n_hidden, vocab_size)
+)
+
+
 def infer(name):
-    model3 = torch.load('ML/models/2023-05-13 215634.pt')
+    model3 = torch.load("ML/models/2023-05-13 215634.pt")
     for layer in model3:
         layer.training = False
     model3.eval()
@@ -58,3 +131,5 @@ def infer(name):
             if nameg not in names_gen:
                 names_gen.append(nameg)
     return names_gen
+
+# ------------------------
