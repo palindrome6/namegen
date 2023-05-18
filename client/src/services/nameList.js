c54d8f5d6d6ce3be1489541bddbccf1bc13b543e
palindrome6 <nitinnarayan1501@gmail.com>
2023-05-18 23:40:27 +0200

    Revert "test to check if fetch is working on mobile"
    This reverts commit cee54ac97c4f8f75179501865283b6bfdfdf923b.

--- client/src/services/nameList.js
+++ client/src/services/nameList.js
@@ -6,28 +6,17 @@ let nameList = [];
 export const getNameList = async (value) => {
     NameListAPIURL.searchParams.set('name', value);
     const url = NameListAPIURL.toString();
-    // const response = axios({
-    //     method: 'get',
-    //     url: url,
-    //     data: {
-    //         name: value,
-    //     },
-    //     headers: {
-    //         "Access-Control-Allow-Origin": "*",
-    //     }
-    // }).catch((err) => {
-    //     console.error(`Could not process request - ${err}`)
-    // })
-    
-    const response1 =  fetch(url, 
-        {
-            method: 'GET',
-            headers: {
-                        "Access-Control-Allow-Origin": "*"},
-            
-            
-
-        })
-        // console.log(response, response1.text());
-    return response1;
+    const response = axios({
+        method: 'get',
+        url: url,
+        data: {
+            name: value,
+        },
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        }
+    }).catch((err) => {
+        console.error(`Could not process request - ${err}`)
+    })
+    return response;
 };
